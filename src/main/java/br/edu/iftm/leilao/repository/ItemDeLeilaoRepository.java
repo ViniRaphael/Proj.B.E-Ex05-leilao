a5e3099b682f97e600efd978ef7348979ad2b65e
Vinicius Raphael <133444109+ViniRaphael@users.noreply.github.com>
2024-07-26 23:56:20 -0300

    Att. Arquivos

--- src/main/java/br/edu/iftm/leilao/repository/ItemDeLeilaoRepository.java
+++ src/main/java/br/edu/iftm/leilao/repository/ItemDeLeilaoRepository.java
@@ -1,11 +1,14 @@
 package br.edu.iftm.leilao.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import br.edu.iftm.leilao.model.ItemDeLeilao;
+import br.edu.iftm.leilao.model.Lance;
 
 @Repository
-public interface ItemDeLeilaoRepository extends CrudRepository<ItemDeLeilao, Long> {
-	
+public interface LanceRepository extends CrudRepository<Lance, Long> {
+	List<Lance> findByParticipanteId(Long id);
+	List<Lance> findByParticipanteIdAndValorGreaterThan(Long id, Double valor);
 }
