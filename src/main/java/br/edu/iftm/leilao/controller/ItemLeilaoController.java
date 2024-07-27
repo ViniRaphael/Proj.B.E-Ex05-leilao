9ea4ddc77c1e07ec5d914f20d9aec9bc864e9bc2
Vinicius Raphael <133444109+ViniRaphael@users.noreply.github.com>
2024-07-26 23:51:21 -0300

    Att. Arquivo

--- src/main/java/br/edu/iftm/leilao/controller/ItemLeilaoController.java
+++ src/main/java/br/edu/iftm/leilao/controller/ItemLeilaoController.java
@@ -3,6 +3,7 @@ package br.edu.iftm.leilao.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,38 +17,45 @@ import br.edu.iftm.leilao.model.ItemDeLeilao;
 import br.edu.iftm.leilao.model.Lance;
 import br.edu.iftm.leilao.service.ItemdeLeilaoService;
 
-
 @RestController
 @RequestMapping("/itemdeleilao")
-public class ItemLeilaoController {
+public class ItemDeLeilaoController {
 
 	@Autowired
-	private ItemdeLeilaoService itemdeLeilaoService;
+	private ItemdeLeilaoService service;
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+	}
+
+	@GetMapping("/{id}")
+	public ItemDeLeilao item(@PathVariable Long id) {
+		return service.busca(id);
+	}
+
+	@PutMapping("/{id}")
+	public ItemDeLeilao atualiza(@PathVariable Long id, @RequestBody ItemDeLeilao item) {
+		return service.atualiza(id, item);
+	}
 
 	@PostMapping
 	public ItemDeLeilao novo(@RequestBody ItemDeLeilao item) {
-		return itemdeLeilaoService.novo(item);
+		return service.salva(item);
 	}
 
 	@GetMapping
 	public List<ItemDeLeilao> itens() {
-		return itemdeLeilaoService.buscarTodos();
-	}
-
-	@PutMapping("/{id}")
-	public ItemDeLeilao atualiza(@RequestBody ItemDeLeilao item, @PathVariable Long id) {
-		return itemdeLeilaoService.atualiza(item, id);
+		return service.itens();
 	}
 
 	@PostMapping("/{id}")
-	public Lance registraLance(@RequestBody Lance lance, @PathVariable Long id) {
-		return itemdeLeilaoService.registraLance(lance, id);
-
+	public ItemDeLeilao registraLance(@RequestBody Lance lance, @PathVariable Long id) {
+		return service.registraLance(lance, id);		
 	}
-
+	
 	@PatchMapping("/{id}")
-	public Lance atualiza(@PathVariable Long id) {
-		return itemdeLeilaoService.atualiza(id);
+	public ItemDeLeilao encerraLeilao(@PathVariable Long id) {
+		return service.encerraLeilao(id);
 	}
-
 }
