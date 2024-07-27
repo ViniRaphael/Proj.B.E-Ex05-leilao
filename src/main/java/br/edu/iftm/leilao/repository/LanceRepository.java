b2c71372b5c82c6d33bdb0212f4e7af63d9f94d5
Vinicius Raphael <133444109+ViniRaphael@users.noreply.github.com>
2024-07-26 23:57:43 -0300

    Att. Arquivos

--- src/main/java/br/edu/iftm/leilao/repository/LanceRepository.java
+++ src/main/java/br/edu/iftm/leilao/repository/LanceRepository.java
@@ -9,7 +9,6 @@ import br.edu.iftm.leilao.model.Lance;
 
 @Repository
 public interface LanceRepository extends CrudRepository<Lance, Long> {
-	List<Lance> findByParticipanteId(Long id);// select * from lance l, participante p where p.id = l.participante.id?
-	//todos os lances do participante com id = 117 e valor maior do que 100
+	List<Lance> findByParticipanteId(Long id);
 	List<Lance> findByParticipanteIdAndValorGreaterThan(Long id, Double valor);
 }
